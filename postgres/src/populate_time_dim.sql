5127b33a4b45404ab8b56fa5b974fafc9539873e
Erick Andrews <andrews_erick@yahoo.com>
2025-02-18 07:04:30 -0800

    updating to only scrape date and season

--- postgres/src/populate_time_dim.sql
+++ postgres/src/populate_time_dim.sql
@@ -3,59 +3,53 @@ BEGIN TRANSACTION;
 WITH row_data AS (
 	SELECT
 		date_of_scraping,
-		season,
-		host_since,
+		season
 		ROW_NUMBER() OVER (
-			PARTITION BY host_since
-			ORDER BY host_since
+			PARTITION BY date_of_scraping
+			ORDER BY date_of_scraping
 		) AS rn
 	FROM listings.airbnb_staging
 ),
 new_data AS (
 	SELECT
 		date_of_scraping,
-		season,
-		host_since
+		season
 	FROM row_data
 	WHERE rn=1
 )
 -- Mark old records as invalid
-UPDATE listings.location_dim d
+UPDATE listings.time_dim d
 SET valid_to = CURRENT_TIMESTAMP,
 	is_current = FALSE
 FROM new_data s
-WHERE d.host_since = s.host_since
+WHERE d.date_of_scraping = s.date_of_scraping
 AND d.is_current = TRUE
 AND (
-	d.date_of_scraping <> s.date_of_scraping
+	AND	d.date_of_scraping <> s.date_of_scraping
 	OR d.season <> s.season
-	OR d.host_since <> s.host_since
 );
 -- Same CTE as above
 WITH row_data AS (
 	SELECT
 		date_of_scraping,
 		season,
-		host_since,
 		ROW_NUMBER() OVER (
-			PARTITION BY host_since
-			ORDER BY host_since
+			PARTITION BY date_of_scraping
+			ORDER BY date_of_scraping
 		) AS rn
 	FROM listings.airbnb_staging
 ),
 new_data AS (
 	SELECT
 		date_of_scraping,
-		season,
-		host_since
+		season
 	FROM row_data
 	WHERE rn=1
 )
 -- Insert where invalid
-INSERT INTO listings.location_dim (
+INSERT INTO listings.time_dim (
 	date_of_scraping,
 	season,
-	host_since,
 	valid_from,
 	valid_to,
 	is_current
@@ -63,19 +57,17 @@ INSERT INTO listings.location_dim (
 SELECT 
 	s.date_of_scraping,
 	s.season,
-	s.host_since,
 	CURRENT_TIMESTAMP,
 	NULL,
 	TRUE
 FROM new_data s
-LEFT JOIN listings.location_dim d
-	ON s.host_since = d.host_since
+LEFT JOIN listings.time_dim d
+	ON s.date_of_scraping = d.date_of_scraping
 	AND d.is_current = TRUE
-WHERE d.host_since IS NULL 
+WHERE d.date_of_scraping IS NULL 
 	OR (
 	d.date_of_scraping <> s.date_of_scraping
 	OR d.season <> s.season
-	OR d.host_since <> s.host_since
 	);
 
 COMMIT TRANSACTION;
